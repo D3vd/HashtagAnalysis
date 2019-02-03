8b4c66775b6fb3358a17457e53e545fd53db7888
R3l3ntl3ss <dev.91096@gmail.com>
2019-02-03 17:35:26 +0530

    Added Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="header">
+    <header>
+      <img src="../assets/logo.png" alt="Hashtag Analysis">
+      <h1>Hashtag Analysis</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Noto+Sans");
+
+header {
+  color: aliceblue;
+  position: relative;
+  overflow: hidden;
+  margin-left: 19%;
+}
+
+header h1 {
+  font-size: 70px;
+  font-family: "Noto Sans", sans-serif;
+  position: relative;
+  left: 10px;
+}
+
+header img {
+  height: 200px;
+  float: left;
+}
+
+@media screen and (max-width: 767px) {
+  header h1 {
+    font-size: 30px;
+    left: 0;
+    padding-top: 5px;
+  }
+
+  header img {
+    height: 100px;
+  }
+
+  header {
+    margin-left: 2%;
+  }
+}
+</style>
+
+
+
